5c3cc512671612c00600eabbd28539006ea30e81
Nicole L. Hopkins <nicolehopkins@pursuit.org>
2019-05-19 13:26:41 -0400

    revising user file

--- services/users.js
+++ services/users.js
@@ -1,24 +1,29 @@
 const {db} = require('./dbConnect');
-const UserService = {};
+const userService = {};
 
 
-UserService.create = (email,token,recentlyViewed) =>{
+// CREATE
+userService.create = (email,token,recentlyViewed) =>{
   return db.none('INSERT INTO users (email,token,recentlyViewed) VALUES (${email}, ${token},${recentlyViewed});',{
     email,
     token,
     recentlyViewed
   });
 }
-UserService.read = (email) =>{
+
+// READ
+userService.read = (email) =>{
   return db.one ('SELECT id from users WHERE email=${email}',{
     email
   });
 }
-UserService.update = (id,recentlyViewed) =>{
+
+//UPDATE
+userService.update = (id,recentlyViewed) =>{
   return db.none('UPDATE users SET recentlyViewed = ${recentlyViewed} WHERE id=${id}',{
     id,
     recentlyViewed
   })
 }
 
-module.exports = {UserService};
+module.exports = userService;
