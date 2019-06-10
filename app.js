a6b1ec7e7949c069c18698912d2ac85782120a54
Tarek <bayridge4th@gmail.com>
2019-06-10 14:59:45 -0400

    edit

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ const favoriteRouter = require('./routes/favorites');
 const groceryRouter = require('./routes/groceries');
 const webscrape = require('./routes/webscrape')
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // BROWSER ROUTES
 app.use('/users', userRouter);
