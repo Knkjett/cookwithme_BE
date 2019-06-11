d0ec10712d6d61597b363b6645810764dd641f4d
Tarek <bayridge4th@gmail.com>
2019-06-11 14:49:29 -0400

    forgot to next the middleware

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(function(req, res, next) {
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    next()
 });
 // app.use(function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "*");
