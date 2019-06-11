828f8154e93d5cf16b82090c8b37c53d10cb621b
Tarek <bayridge4th@gmail.com>
2019-06-11 15:07:28 -0400

    try again to fix cor

--- app.js
+++ app.js
@@ -16,6 +16,14 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+  });
+  
+
 // ROUTERS
 const userRouter = require('./routes/users');
 const recipeRouter = require('./routes/recipes');
