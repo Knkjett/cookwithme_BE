86a55151bd4c55ca83ca55290f952ac6c43678df
Tarek <bayridge4th@gmail.com>
2019-06-10 21:08:42 -0400

    added cors

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 // MIDDLEWARE
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-//app.use(cors());
+app.use(cors());
 
 // ROUTERS
 const userRouter = require('./routes/users');
@@ -15,12 +15,12 @@ const favoriteRouter = require('./routes/favorites');
 const groceryRouter = require('./routes/groceries');
 const webscrape = require('./routes/webscrape')
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//     next();
+// });
 
 // BROWSER ROUTES
 app.use('/users', userRouter);
