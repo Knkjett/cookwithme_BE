4b2609605fec761ac9190af3bd654ba4ec04478b
Tarek <bayridge4th@gmail.com>
2019-06-11 15:19:59 -0400

    changed header to setHeader

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
-  });
+  }); 
   
 
 // ROUTERS
