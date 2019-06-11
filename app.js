0f96319700c70b386b4d2b9f4e385b03bddacd05
Tarek <bayridge4th@gmail.com>
2019-06-11 15:24:23 -0400

    again try fix cors

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 const express = require('express')
-  , cors = require('cors')
-  , app = express();
-app.options('*', cors());
+const app = express();
 const bodyParser = require('body-parser');
-// const cors = require('cors')
+const cors = require('cors')
 
-// MIDDLEWARE
-// app.use(function(req, res, next) {
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//     res.setHeader("Access-Control-Allow-Credentials", "true");
-//     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-//     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-//     next()
-// });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-app.use(cors());
+//app.use(cors());
 
 app.use(function(req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
