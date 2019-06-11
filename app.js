5444b42de32b363f8e1f89d7eb7ca3aee19ef20e
Tarek <bayridge4th@gmail.com>
2019-06-11 14:57:09 -0400

    commented out cors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const cors = require('cors')
+//const cors = require('cors')
 
 // MIDDLEWARE
 app.use(function(req, res, next) {
