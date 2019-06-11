c990d49668d6b5b466c73c00416eaaea2618d377
Tarek <bayridge4th@gmail.com>
2019-06-11 15:02:59 -0400

    put back cors

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ const bodyParser = require('body-parser');
 const cors = require('cors')
 
 // MIDDLEWARE
-app.use(function(req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-    next()
-});
+// app.use(function(req, res, next) {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Credentials", "true");
+//     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+//     next()
+// });
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-//app.use(cors());
+app.use(cors());
 
 // ROUTERS
 const userRouter = require('./routes/users');
@@ -23,13 +23,6 @@ const favoriteRouter = require('./routes/favorites');
 const groceryRouter = require('./routes/groceries');
 const webscrape = require('./routes/webscrape')
 
-
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-//     next();
-// });
 
 // BROWSER ROUTES
 app.use('/users', userRouter);
