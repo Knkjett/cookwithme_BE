3f2793930ef17aeb707be0c5b8d50d65c00865d7
Tarek <bayridge4th@gmail.com>
2019-06-11 15:33:49 -0400

    added app options

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
 const express = require('express')
 const app = express();
+app.options('*', cors())
 const bodyParser = require('body-parser');
 const cors = require('cors')
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-//app.use(cors());
+app.use(cors());
 
-app.use(function(req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-  }); 
   
-
 // ROUTERS
 const userRouter = require('./routes/users');
 const recipeRouter = require('./routes/recipes');
