aca6eb1e447e4d8877af9453e40a1a76ff2da24d
Tarek <bayridge4th@gmail.com>
2019-06-11 14:20:38 -0400

    edit app

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const webscrape = require('./routes/webscrape')
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
     next();
 });
